bda80bc943f31b87a5c98d9ff9383221b6c73a97
dnlg88 <92231783+dnlg88@users.noreply.github.com>
2022-04-20 21:57:31 +0200

    new features
    Refactored the context, added User page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 
@@ -16,8 +18,17 @@ function App() {
             <Navbar />
             <main className="container mx-auto px-3 mb-12">
               <Routes>
-                <Route path="/" element={<Home />} />
+                
+
+                <Route path="/" element={
+                  <> 
+                  <Alert />
+                  <Home />
+                  </>
+                  }
+                />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
